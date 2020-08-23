8e94c66345e2c413d22b8e5fb13f7bb993cde436
Glitch (imported-melted-nest) <none>
2020-08-23 16:11:18 +0000

    app funciona offline con workbox

--- service-worker.js
+++ service-worker.js
@@ -25,61 +25,22 @@ workbox.core.setCacheNameDetails({
 });
 
 workbox.precaching.precacheAndRoute([
-  "/offline.html",// Archivo html que muestra mensaje de falta de conexión
-])
+'offline.html',
+'manifest.json']);
 
-"use strict";
 
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = "static-cache-v1";
 
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = ["/offline.html"];
 
-self.addEventListener("install", evt => {
-  console.log("[ServiceWorker] Install");
-  // CODELAB: Precache static resources here.
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("[ServiceWorker] Pre-caching offline page");
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-
-  self.skipWaiting();
+self.addEventListener('fetch', event => {
+event.respondWith(
+caches.open('my-app-precache-cache-v1').then(cache => {
+return cache.match(event.request).then(cachedResponse => {
+const fetchPromise = fetch(event.request).then(response => {
+cache.put(event.request, response.clone());
+return response;
+});
+return cachedResponse || fetchPromise;
+})
+}));
 });
 
-self.addEventListener("activate", evt => {
-  console.log("[ServiceWorker] Activate");
-  // CODELAB: Remove previous cached data from disk.
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME) {
-            console.log("[ServiceWorker] Removing old cache", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", evt => {
-  console.log("[ServiceWorker] Fetch", evt.request.url);
-  // CODELAB: Add fetch event handler here.
-  if (evt.request.mode !== "navigate") {
-    // Not a page navigation, bail.
-    return;
-  }
-  evt.respondWith(
-    fetch(evt.request).catch(() => {
-      return caches.open(CACHE_NAME).then(cache => {
-        return cache.match("offline.html");
-      });
-    })
-  );
-});
